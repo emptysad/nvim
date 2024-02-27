89a196bb04d74ad28bd1e19a850a4c3982b90c23
emptysad <emptysad@proton.me>
2024-02-27 19:35:59 +0600

    remapping next and prev targets from enter and tab to c-j and c-h

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -2,5 +2,11 @@ return {
     'ggandor/leap.nvim',
     config = function()
         require('leap').create_default_mappings()
+        require('leap').opts.special_keys = {
+            next_target = '<C-j>',
+            prev_target = '<C-h>',
+            next_group = '<space>',
+            prev_group = '<tab>',
+        }
     end
 }
