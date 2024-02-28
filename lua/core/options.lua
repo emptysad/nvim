84eba5decee44c214f991dadcf92e6089165ea30
emptysad <emptysad@proton.me>
2024-02-28 23:04:19 +0600

    right align absolute number of current line

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -9,6 +9,8 @@ vim.opt.sidescrolloff = 5
 vim.opt.undofile = true
 vim.opt.virtualedit = 'block'
 vim.opt.wildmode = 'longest:full,full'
+vim.opt.statuscolumn = '%=%{v:relnum?v:relnum:v:lnum} '
+vim.opt.numberwidth = 4
 
 -- Tab
 vim.opt.tabstop = 4                 -- number of visual spaces per TAB
