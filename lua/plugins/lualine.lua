73fee751a17b8ecad1f2075b8d4322b4e64e289e
emptysad <emptysad@proton.me>
2024-02-28 23:09:51 +0600

    beautiful fading on edges of lualine I'm genius

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -22,12 +22,12 @@ return {
                 }
             },
             sections = {
-                lualine_a = {'mode'},
+                lualine_a = { {'mode', separator = { left = ' ░▒' }, right_padding = 2 } },
                 lualine_b = {'branch', 'diff', 'diagnostics'},
                 lualine_c = {'filename'},
                 lualine_x = {'encoding', 'fileformat', 'filetype'},
                 lualine_y = {'progress'},
-                lualine_z = {'location'}
+                lualine_z = { {'location', separator = { right = '▒░ ' }, left_padding = 2 } }
             },
             inactive_sections = {
                 lualine_a = {},
