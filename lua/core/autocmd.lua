9ae39912a015f9f63e1c3e0b937847f9c71f0832
emptysad <emptysad@proton.me>
2024-03-08 17:40:24 +0500

    add gitsigns plugin + try of highlightning

--- lua/core/autocmd.lua
+++ lua/core/autocmd.lua
@@ -1,29 +1,39 @@
 local autocmd = vim.api.nvim_create_autocmd
 
 local function augroup(name, fnc)
-    fnc(vim.api.nvim_create_augroup(name, { clear = true }))
+  fnc(vim.api.nvim_create_augroup(name, { clear = true }))
 end
 
 augroup("AlphaHighlightning", function(group)
-    autocmd("VimEnter", {
-        group = group,
-        callback = function()
-            vim.cmd[[highlight Startlogo1    guifg='#ffc800']]
-            vim.cmd[[highlight Startlogo2    guifg='#ffaa20']]
-            vim.cmd[[highlight Startlogo3    guifg='#fe8c34']]
-            vim.cmd[[highlight Startlogo4    guifg='#f37143']]
-            vim.cmd[[highlight Startlogo5    guifg='#e3584f']]
-            vim.cmd[[highlight Startlogo6    guifg='#ce4357']]
-            vim.cmd[[highlight Startlogo7    guifg='#b4335d']]
-            vim.cmd[[highlight Startlogo8    guifg='#98285e']]
-            vim.cmd[[highlight Startlogo9    guifg='#79215c']]
-            vim.cmd[[highlight Startlogo10   guifg='#5b1c55']]
-            vim.cmd[[highlight Startlogo11   guifg='#3d174a']]
-            vim.cmd[[highlight Startlogo12   guifg='#80308d']]
-        end
-    })
+  autocmd("VimEnter", {
+    group = group,
+    callback = function()
+      vim.cmd[[highlight Startlogo1    guifg='#ffc800']]
+      vim.cmd[[highlight Startlogo2    guifg='#ffaa20']]
+      vim.cmd[[highlight Startlogo3    guifg='#fe8c34']]
+      vim.cmd[[highlight Startlogo4    guifg='#f37143']]
+      vim.cmd[[highlight Startlogo5    guifg='#e3584f']]
+      vim.cmd[[highlight Startlogo6    guifg='#ce4357']]
+      vim.cmd[[highlight Startlogo7    guifg='#b4335d']]
+      vim.cmd[[highlight Startlogo8    guifg='#98285e']]
+      vim.cmd[[highlight Startlogo9    guifg='#79215c']]
+      vim.cmd[[highlight Startlogo10   guifg='#5b1c55']]
+      vim.cmd[[highlight Startlogo11   guifg='#3d174a']]
+      vim.cmd[[highlight Startlogo12   guifg='#80308d']]
+    end
+  })
 end)
 
+augroup("GitSignsHighlightning", function(group)
+  autocmd("VimEnter", {
+    group = group,
+    callback = function()
+      vim.cmd[[highlight GitSignsAdd        guifg='#4bf542']]
+      vim.cmd[[highlight GitSignsChange     guifg='#f5ef42']]
+      vim.cmd[[highlight GitSignsDelete     guifg='#f54242']]
+    end
+  })
+end)
 --
 -- augroup("AutoStartNvimTree", function(group)
 --     autocmd("VimEnter", {
@@ -39,12 +49,12 @@ end)
 -- end)
 
 augroup("NvimTreeStatusColumn", function(group)
-    autocmd("BufEnter", {
-        group = group,
-        callback = function()
-            if vim.bo.filetype == 'NvimTree' then
-                vim.wo.statuscolumn = ""
-            end
-        end
-    })
+  autocmd("BufEnter", {
+    group = group,
+    callback = function()
+      if vim.bo.filetype == 'NvimTree' then
+        vim.wo.statuscolumn = ""
+      end
+    end
+  })
 end)
