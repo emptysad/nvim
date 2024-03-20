132118fd6787fb2c9cf55887150cc7b8e21f39a8
emptysad <emptysad@proton.me>
2024-03-21 01:15:35 +0500

    add Lua settings from Deponian config of nvim-lspconfig

--- lua/plugins/lsp/nvim-lspconfig.lua
+++ lua/plugins/lsp/nvim-lspconfig.lua
@@ -6,6 +6,34 @@ return {
 
 		lspconfig.lua_ls.setup({
 			capabilities = capabilities,
+			settings = {
+				Lua = {
+					format = {
+						enable = true,
+						defaultConfig = {
+							indent_style = "space",
+							indent_size = "2",
+						},
+					},
+					runtime = {
+						-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+						version = "LuaJIT",
+					},
+					diagnostics = {
+						-- Get the language server to recognize the `vim` global
+						globals = { "vim" },
+					},
+					workspace = {
+						-- Make the server aware of Neovim runtime files
+						library = vim.api.nvim_get_runtime_file("", true),
+						checkThirdParty = false,
+					},
+					-- Do not send telemetry data containing a randomized but unique identifier
+					telemetry = {
+						enable = false,
+					},
+				},
+			},
 		})
 
 		vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
