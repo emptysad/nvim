c211acff84cfb6cb4a84c41a5cdfd6facc94b9ad
emptysad <emptysad@proton.me>
2024-04-04 23:33:28 +0500

    change gfl hotkey for format to gf

--- lua/plugins/lsp/nvim-lspconfig.lua
+++ lua/plugins/lsp/nvim-lspconfig.lua
@@ -39,6 +39,6 @@ return {
 		vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 		vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-		vim.keymap.set("n", "<leader>gfl", vim.lsp.buf.format, {})
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
 	end,
 }
