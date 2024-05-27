fcd4be95c6a5c8b4f828e123bccb9b8931ae366a
emptysad <emptysad@proton.me>
2024-05-27 17:51:32 +0500

    delete autotag in nvim-treesitter config

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -2,9 +2,6 @@ return {
   "nvim-treesitter/nvim-treesitter",
   config = function ()
     require'nvim-treesitter.configs'.setup {
-      autotag = {
-        enable = true,
-      },
       auto_install = true,
       highlight = {
         enable = true,
