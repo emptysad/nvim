6d32289cdc1aa3e8206ee5a0485fef4d60f086f7
emptysad <emptysad@proton.me>
2024-05-27 17:57:46 +0500

    add list of plugins for Lazy for better debugging in future

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -11,4 +11,5 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({{import = "plugins"}, {import = "plugins.lsp"}})
+-- require("lazy").setup({{import = "plugins"}, {import = "plugins.lsp"}})
+require("lazy").setup("core.list-of-plugins")
