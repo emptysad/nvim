4c0ed24a2d4e923347b980a0b24ab0dc9e1a00b2
emptysad <emptysad@proton.me>
2024-05-27 17:56:18 +0500

    delete neocolumn (problems with undotree) and add smartcolumn

---
+++ lua/plugins/smartcolumn.lua
@@ -0,0 +1,9 @@
+return {
+	"m4xshen/smartcolumn.nvim",
+	config = {
+		colorcolumn = "80",
+		disabled_filetypes = {"alpha", "NvimTree", "lazy", "mason", "help", "checkhealth", "lspinfo", "noice", "Trouble", "fish", "zsh"},
+		custom_colorcolumn = {},
+		scope = "file",
+	},
+}
