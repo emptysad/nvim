3a9e7345202ad6773d1abd97deec92e4cae5e7ba
emptysad <emptysad@proton.me>
2024-06-20 16:22:38 +0500

    dirty fix opening nvim-tree in auto-session

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -4,6 +4,17 @@ return {
     require("auto-session").setup({
       log_level = "error",
       auto_session_suppress_dirs = { "~/", "~/ITSTEPACADEMY/Work", "~/Downloads", "/", "~/.config/nvim" },
+      pre_save_cmds = {
+        function()
+          vim.cmd([[tabdo lua require("nvim-tree.api").tree.close()]])
+        end,
+      },
+      post_restore_cmds = {
+        function()
+          vim.cmd([[tabdo lua require("nvim-tree.api").tree.close()]])
+          vim.cmd([[tabdo lua require("nvim-tree.api").tree.open({find_file=true})]])
+        end,
+      },
     })
   end,
 }
